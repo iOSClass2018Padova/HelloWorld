4df838356ee5c3169282f36dfc42daf3df006881
Stefano Vecchiati <stefano.vecchiati@aol.com>
2018-09-18 10:01:14 +0200

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by stefano vecchiati on 18/09/2018.
+//  Copyright © 2018 co.eggon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
